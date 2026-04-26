14534ec172b03309ab8e22df104eee6a98c215dd
eslam <eslam.ismail2003@gmail.com>
2026-04-26 06:02:32 +0200

    Added config and connection to front end layered page

--- src/main/java/com/elearning/ELearningPlatform/config/CorsConfig.java
+++ src/main/java/com/elearning/ELearningPlatform/config/CorsConfig.java
@@ -12,7 +12,9 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**").allowedOrigins("http://localhost:3000").allowedMethods("GET", "POST", "PUT", "DELET");
+                registry.addMapping("/api/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
     }
